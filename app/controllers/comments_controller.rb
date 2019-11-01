1eebfe669e3827886b3f23eb3c28024e02957474
mubi-janjua <mubeenaftabjanjua@gmail.com>
2019-11-01 12:21:03 +0500

    new project

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+	def create
+		@blog = Blog.find_by(params[:blog_id])
+		@comment = @blog.comments.create(comment_params)
+
+		if @comment.save
+			redirect_to @blog
+		end
+	end
+
+	def destroy
+		@blog = Blog.find(params[:blog_id])
+		@comment = Comment.find(params[:id])
+		@comment.destroy
+
+		redirect_to @blog
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:comment, :user_id)
+	end
+end
---
+++ app/models/blog.rb
@@ -0,0 +1,4 @@
+class Blog < ApplicationRecord
+	has_many :comments
+	has_many :users, through: :comments
+end
